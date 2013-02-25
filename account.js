a5ec95f3d94598cec4555942ffea73ccd5976883
naoki kodama <naoki.koda@gmail.com>
2013-02-26 00:30:45 +0900

    Add display list after add a item.

--- account.js
+++ account.js
@@ -5,23 +5,22 @@
 var host = 'http://localhost:3000/',
     request = require('request'),
     params = process.argv.slice(2),
-    item
-;
+    item;
 
-switch(params[0]) {
-case 'add':
-  item = { amount: params[1] };
-  if (typeof params[2] !== 'undefined') {
-    item.date = new Date(params[2]+'-00:00:00').getTime();
+function addItem(amount, date, callback){
+  item = { amount: amount };
+  if (typeof date !== 'undefined') {
+    item.date = new Date(date + '-00:00:00').getTime();
   }
   request.post(host + 'add', {form: item}, function(error, response, body) {
     if (error) {
       console.log("エラーだよ.");
     }
+    if (typeof callback !== 'undefined') { callback(); }
   });
-  break;
+}
 
-case 'list':
+function showList(){
   request.get(host + 'list', function(error, response, body) {
     var result = JSON.parse(response.body);
     result.forEach(function(item) {
@@ -29,13 +28,13 @@ case 'list':
       console.log("(" + d.getFullYear() + '/' + (d.getMonth() + 1) + '/' + d.getDate() + ") " + item.amount + '円');
     });
   });
-  break;
-
-case 'month':
+}
+ 
+function showMonth(month){
   item = {};
-  if (typeof params[1] !== 'undefined') {
+  if (typeof month !== 'undefined') {
     var year = new Date().getFullYear(),
-        yyyymm = year + ('0' + parseInt(params[1], 10)).slice(-2);
+        yyyymm = year + ('0' + parseInt(month, 10)).slice(-2);
     request.get(host + 'month?month=' + yyyymm, function(error, response, body) {
       var result = JSON.parse(response.body), sum = 0;
       result.forEach(function(item) {
@@ -47,7 +46,22 @@ case 'month':
   else {
     console.log("ex) node pocket month 12");
   }
+}
+ 
+switch(params[0]) {
+case 'add':
+  addItem(params[1], params[2], function(){
+    showList()
+  });
   break;
+
+case 'list':
+  showList();
+  break;
+
+case 'month':
+  showMonth(params[1]);
+ break;
 
 default:
   console.log("Usage)\n node pocket [add|list|month] params\n");
